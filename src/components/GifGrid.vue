5708538abd69e14529830a2b26536b15c4fc1c19
joshlapid <lapidjoshua@gmail.com>
2018-12-02 16:59:13 -0800

    Styling updates

---
+++ src/components/GifGrid.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <div class="grid">
+		<div class="grid-item" v-for="gif in gifs" v-bind:style="{ 'background-image': 'url(' + gif.images.original.url + ')' }">
+			<div class="grid-info">
+				<span v-if="gif.user">{{ gif.user.display_name }}</span><br />
+				Click to Vue
+			</div>
+		</div>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GifGrid',
+  props: {
+    msg: String,
+    gifs: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.grid {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+}
+
+.grid-item {
+	background-position: center;
+	background-size: cover;
+	width: 220px;
+	height: 180px;
+	margin: 10px;
+}
+
+.grid-info {
+	height: 100%;
+	color: #ffffff;
+	background: rgba(0,0,0,0.7);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	opacity: 0;
+	transition: 0.3s;
+	font-size: 12px;
+	flex-wrap: wrap;
+
+	&:hover {
+		opacity: 1;
+		transition: 0.3s;
+	}
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,134 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  	<div class="header">
+  		<img src="../assets/logo.png"><h1>VUE SOME GIFS</h1>
+  	</div>
+  	<input type="text" class="input" name="query" v-model="query" @keyup="searchGifs" placeholder="Search all of the gifs">
+  	<GifGrid v-bind:gifs="searchedGifs" msg="Welcome to Your Vue.js App"/>
+  	<button v-if="query" @click="loadMoreGifs">Load More</button>
+    <h1>TRENDING</h1>
+    <GifGrid v-bind:gifs="trendingGifs" msg="Welcome to Your Vue.js App"/>
+	<button @click="loadMoreTrending">Load More</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import GifGrid from '@/components/GifGrid.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    GifGrid
+  },
+  data() {
+  	return {
+  		apiUrl: 'http://api.giphy.com/v1/gifs',
+        apiKey: '80bfcbf357864cd18518c324f47a7098',
+        trendingGifs: null,
+        searchedGifs: null,
+        searchButton: false,
+        query: '',
+        limit: 10,
+        offset: null
+  	}
+  },
+  methods	: {
+  	fetchTrendingGifs: function() {
+      const url = `${this.apiUrl}/trending?api_key=${this.apiKey}&limit=${this.limit}`;
+
+      axios.get(url)
+        .then(response => this.trendingGifs = response.data.data)
+    },
+
+    searchGifs: function() {
+	  const url = `${this.apiUrl}/search?api_key=${this.apiKey}&q=${this.query}&limit=${this.limit}`;
+	  this.searchButton = true;
+	  axios.get(url)
+        .then(response => this.searchedGifs = response.data.data)
+	},
+
+	loadMoreTrending: function() {
+	  this.offset += 10;
+
+	  const url = `${this.apiUrl}/trending?api_key=${this.apiKey}&limit=${this.limit}&offset=${this.offset}`;
+
+	  axios.get(url)
+        .then(response => {
+        	let i = 0;
+	        let loadTrending = response.data.data;
+        	for(i = 0; i < this.limit; i++ ) {
+        		let addloadTrending = loadTrending[i];
+	        	this.trendingGifs.push(addloadTrending);
+        	}
+        })
+	},
+
+	loadMoreGifs: function() {
+	  this.offset += 10;
+
+	  const url = `${this.apiUrl}/search?api_key=${this.apiKey}&q=${this.query}&limit=${this.limit}&offset=${this.offset}`;
+
+	  axios.get(url)
+        .then(response => {
+        	let i = 0;
+	        let loadGif = response.data.data;
+        	for(i = 0; i < this.limit; i++ ) {
+        		let addLoadGif = loadGif[i];
+	        	this.searchedGifs.push(addLoadGif);
+        	}
+        })
+	}
+  },
+  created: function() {
+    this.fetchTrendingGifs();
+    this.searchGifs();
   }
 }
+
+
 </script>
+
+<style lang="scss">
+body {
+	background: rgb(18, 18, 18);
+	color: #ffffff;
+}
+
+h1 {
+	color: #ffffff;
+	text-align: left;
+}
+
+input {
+	width: 100%;
+	font-size: 3em;
+	padding: 5px 10px;
+	border-width: 0px;
+	margin-bottom: 30px;
+}
+
+button {
+	border-radius: 0;
+	background: #000000;
+	color: #ffffff;
+	border-width: 0;
+}
+
+.home {
+	max-width: 1200px;
+	margin: 0 auto;
+}
+
+.header {
+	display: flex;
+	align-items: center;
+
+	img	{
+		margin-right: 20px;
+		width: 50px;
+		height: 50px;
+	}
+}
+</style>
